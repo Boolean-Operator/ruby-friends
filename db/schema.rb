d61fd43874b09ce5049da49fb49b155d5b9a15a9
Mark Graybill <marktgraybill@gmail.com>
2021-07-14 00:44:54 -0400

    associated friends to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_031301) do
+ActiveRecord::Schema.define(version: 2021_07_14_041507) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
